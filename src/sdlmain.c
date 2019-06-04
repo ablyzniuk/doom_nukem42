f0aafe2dfcfef3c3cce5b1659beef0364aff4310
azhell <azhell4ik@gmail.com>
2019-06-04 14:16:01 +0300

    gravity

--- src/sdlmain.c
+++ src/sdlmain.c
@@ -39,6 +39,7 @@ void    ft_sdlloop(t_main *m)
 	{
 		SDL_FillRect(m->sdl.winsurface, NULL, 0x000000);
 		ft_gravity(m);
+		ft_de_gravity(m);
 		ft_draw_sky(m);
 		ft_draw_map(m);
 		ft_transform(m);
